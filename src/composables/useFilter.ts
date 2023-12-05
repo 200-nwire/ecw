f5c75a073f20c81b1b115738506aa7a2e2038144
Yosef Balter <yosefb@200apps.co>
2023-12-05 13:02:43 +0200

    build

---
+++ src/composables/useFilter.ts
@@ -0,0 +1,40 @@
+export const sources = [
+    {
+      name: 'פייסבוק',
+      value: 'facebook',
+    },
+    {
+      name: 'אתר',
+      value: 'web',
+    },
+    {
+      name: 'גוגל',
+      value: 'google',
+    },
+    {
+      name: 'crm',
+      value: 'crm',
+    },
+    {
+      name: 'אפליקציה',
+      value: 'mobile',
+    },
+]
+export const statuses = [
+    {
+      name: 'רכב פעיל',
+      value: 'ACTIVE',
+    },
+    {
+      name: 'רכב לא שילם',
+      value: 'UNPAID',
+    },
+    {
+      name: 'רכב מבוטל מהחודש הבא',
+      value: 'PAUSED',
+    },
+    {
+      name: 'רכב מבוטל',
+      value: 'CANCELED',
+    },
+]
